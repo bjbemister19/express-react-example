05bc6262bd4b2f8e3dd730d9b224215ec4df755b
= <=>
2022-10-09 19:30:50 -0230

    Created server to serve express app

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log('Hello, world')
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT || 3001
+
+app.use('/', express.static('../client/build'))
+
+app.listen(port, () => {
+    console.log(`port ${port}, im listening...`)
+})
+
