dd856c9f8b720acd295f530e5da93464bd8775a8
= <=>
2022-10-09 21:04:42 -0230

    fix pathname to build folder

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const path = require("path")
 
 const app = express()
-
 const port = process.env.PORT || 3001
 
-app.use('/', express.static('../client/build'))
+const static_dir = path.resolve(path.join(path.basename(__dirname), 'client/build'))
+
+app.use('/', express.static(static_dir))
 
 app.listen(port, () => {
     console.log(`port ${port}, im listening...`)
