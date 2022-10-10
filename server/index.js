f0f468453300ffe802df16d8be1564543dd141d4
= <=>
2022-10-09 22:20:49 -0230

    Added logger

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
 const express = require('express')
-const { stat } = require('fs')
 const path = require("path")
+const logger = require('./logger')
 
 const app = express()
 const port = process.env.PORT || 3001
 
 const static_dir = path.resolve(path.join(__dirname, '../client/build'))
-console.log(static_dir)
 
 app.use('/', express.static(static_dir))
 
 app.listen(port, () => {
-    console.log(`port ${port}, im listening...`)
+    logger.get().info(`port ${port}, im listening...`)
 })
 
