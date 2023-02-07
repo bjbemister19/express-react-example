e38ee1b8b2b4fc7fd172adefa47ee2bdc1640af2
Brandon Bemister <bjbemister@gmail.com>
2023-02-06 21:21:24 -0330

    Improve example

--- server/index.js
+++ server/index.js
@@ -9,6 +9,16 @@ const static_dir = path.resolve(path.join(__dirname, '../client/build'))
 
 app.use('/', express.static(static_dir))
 
+app.get('/*', (req, res, next) => {
+  if (req.url.startsWith('/api/')) {
+    next();
+    return;
+  }
+  res.sendFile(path.join(static_dir, 'index.html'));
+});
+
+app.get('/api/authenticate-user', ()=> {})
+
 app.listen(port, () => {
     logger.get().info(`port ${port}, im listening...`)
 })
