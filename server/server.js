92a3c669a2521fedeae72aca6fd972a0a57e20a7
Brandon Bemister <brandon@getmysa.com>
2023-02-17 16:54:49 -0330

    Add postman collection

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3001
 
 const static_dir = path.resolve(path.join(__dirname, '../client/build'))
 
+app.use(express.json())
 app.use('/', express.static(static_dir))
 
 app.get('/*', (req, res, next) => {
@@ -17,7 +18,10 @@ app.get('/*', (req, res, next) => {
   res.sendFile(path.join(static_dir, 'index.html'));
 });
 
-app.get('/api/authenticate-user', ()=> {})
+app.post('/api/echo', (req, res) => {
+  console.log(req);
+  res.json(req.body);
+})
 
 app.listen(port, () => {
     logger.get().info(`port ${port}, im listening...`)
